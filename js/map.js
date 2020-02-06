e5014215245895745be3e43f7065cb3e2e1b5280
Terentiev_SV <TerentievSV@samnipineft.ru>
2020-02-06 11:48:57 +0400

    Формирование модуля Form

--- js/map.js
+++ js/map.js
@@ -82,107 +82,16 @@
     }
   };
 
-  var adForm = document.querySelector('.notice .ad-form');
-  var mainMapPin = mapSection.querySelector('.map__pins .map__pin--main');
-
-  var fillAddress = function (isActivate) {
-    var address = adForm.querySelector('#address');
-
-    var width = mainMapPin.offsetWidth;
-    var height = mainMapPin.offsetHeight;
-    var pinHeight = parseInt(window.getComputedStyle(mainMapPin, ':after').height, 10);
-
-    var positionX = mainMapPin.offsetLeft + 0.5 * width;
-
-    if (!isActivate) {
-      // отображение адреса до активации
-      address.value = positionX + '; ' + (mainMapPin.offsetTop + 0.5 * height);
-      return;
-    }
-
-    // отображение адреса после активации
-    address.value = positionX + '; ' + (mainMapPin.offsetTop + height + pinHeight);
-  };
-
-  var roomNumberSelector = document.querySelector('#room_number');
-  var guestNumberSelector = document.querySelector('#capacity');
-
-  var makeGuestRoomsValidation = function (roomSelector, guestSelector) {
-    var roomNumber = parseInt(
-        roomNumberSelector.options[roomNumberSelector.selectedIndex].value,
-        10
-    );
-    var guestNumber = parseInt(guestNumberSelector
-      .options[guestNumberSelector.selectedIndex]
-      .value, 10);
-    var message = '';
-
-    switch (roomNumber) {
-      case 1:
-      case 2:
-      case 3:
-        if (guestNumber > roomNumber || guestNumber === 0) {
-          message = 'В ' + roomNumber + '-комнатный номер количество гостей не более ' +
-            roomNumber + ' и не менее 1';
-        }
-        break;
-
-      case 100:
-        if (guestNumber !== 0) {
-          message = '100-комнатный номер не для гостей';
-        }
-    }
-
-    roomSelector.setCustomValidity(message);
-    guestSelector.setCustomValidity(message);
-  };
-
-  var adFormHeader = adForm.querySelector('.ad-form-header');
-  var adFormElements = adForm.querySelectorAll('.ad-form__element');
-  var mapCheckFilter = mapSection.querySelector('.map__filters .map__features');
-  var mapSelectFilters = mapSection.querySelectorAll('.map__filters .map__filter');
-
-  var enableForms = function () {
-    // Блокирование формы объявления
-    adFormHeader.removeAttribute('disabled');
-    for (var i = 0; i < adFormElements.length; ++i) {
-      adFormElements[i].removeAttribute('disabled');
-    }
-
-    // Блокирование формы фильтров
-    mapCheckFilter.removeAttribute('disabled');
-    for (var j = 0; j < mapSelectFilters.length; ++j) {
-      mapSelectFilters[j].removeAttribute('disabled');
-    }
-
-    mapSection.classList.remove('map--faded');
-
-    makeGuestRoomsValidation(roomNumberSelector, guestNumberSelector);
-  };
-
   var activateMap = function () {
     renderMapPins();
 
-    enableForms();
-    fillAddress(true);
+    window.form.enableForms();
+    window.form.fillAddress(true);
 
     mapPinsContainer.addEventListener('click', onMapPinsContainerClick);
   };
 
-  var disableForms = function () {
-    // Блокирование формы объявления
-    adFormHeader.setAttribute('disabled', 'true');
-    for (var i = 0; i < adFormElements.length; ++i) {
-      adFormElements[i].setAttribute('disabled', 'true');
-    }
-
-    // Блокирование формы фильтров
-    mapCheckFilter.setAttribute('disabled', 'true');
-    for (var j = 0; j < mapSelectFilters.length; ++j) {
-      mapSelectFilters[j].setAttribute('disabled', 'true');
-    }
-  };
-
+  var mainMapPin = mapSection.querySelector('.map__pins .map__pin--main');
   mainMapPin.addEventListener('mousedown', function (evt) {
     window.utils.isLeftMouseButtonEvent(evt, activateMap);
   });
@@ -191,71 +100,11 @@
     window.utils.isEnterEvent(evt, activateMap);
   });
 
-  roomNumberSelector.addEventListener('change', function () {
-    makeGuestRoomsValidation(roomNumberSelector, guestNumberSelector);
-  });
+  window.form.disableForms();
+  window.form.fillAddress(false);
 
-  guestNumberSelector.addEventListener('change', function () {
-    makeGuestRoomsValidation(roomNumberSelector, guestNumberSelector);
-  });
-
-  var buildingTypeSelector = document.querySelector('#type');
-  var priceField = document.querySelector('#price');
-
-  var makeTypeMinPriceValidation = function () {
-    var typeValue = buildingTypeSelector.options[buildingTypeSelector.selectedIndex].value;
-
-    var minPrice;
-    switch (typeValue) {
-      // Бунгало
-      case window.data.BUILDING_TYPES[3]:
-        minPrice = 0;
-        break;
-
-      // Квартира
-      case window.data.BUILDING_TYPES[1]:
-        minPrice = 1000;
-        break;
-
-      // Дом
-      case window.data.BUILDING_TYPES[2]:
-        minPrice = 5000;
-        break;
-
-      // Дворец
-      case window.data.BUILDING_TYPES[0]:
-        minPrice = 10000;
-        break;
-    }
-
-    priceField.placeholder = minPrice;
-    priceField.min = minPrice;
-  };
-
-  buildingTypeSelector.addEventListener('change', function () {
-    makeTypeMinPriceValidation();
-  });
-
-  priceField.addEventListener('change', function () {
-    makeTypeMinPriceValidation();
-  });
-
-  var makeTimeinTimeoutValidation = function (activeSelector, passiveSelector) {
-    passiveSelector.selectedIndex = activeSelector.selectedIndex;
-  };
-
-  var timeinSelector = document.querySelector('#timein');
-  var timeoutSelector = document.querySelector('#timeout');
-
-  timeinSelector.addEventListener('change', function () {
-    makeTimeinTimeoutValidation(timeinSelector, timeoutSelector);
-  });
-  timeoutSelector.addEventListener('change', function () {
-    makeTimeinTimeoutValidation(timeoutSelector, timeinSelector);
-  });
-
-  disableForms();
-  fillAddress(false);
-  makeGuestRoomsValidation(roomNumberSelector, guestNumberSelector);
-  makeTypeMinPriceValidation();
+  var roomNumberSelector = document.querySelector('#room_number');
+  var guestNumberSelector = document.querySelector('#capacity');
+  window.form.makeGuestRoomsValidation(roomNumberSelector, guestNumberSelector);
+  window.form.makeTypeMinPriceValidation();
 })();
