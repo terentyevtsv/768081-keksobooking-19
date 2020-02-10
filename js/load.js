0fcf43daee582f320208009deac349ee8454273c
Terentiev_SV <TerentievSV@samnipineft.ru>
2020-02-10 10:00:29 +0400

    Добавил и подключил модуль загрузки данных

---
+++ js/load.js
@@ -0,0 +1,32 @@
+'use strict';
+(function () {
+  var StatusCode = {
+    OK: 200
+  };
+  window.load = function (url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', url);
+    xhr.send();
+  };
+})();
